1e29ea9269921a2ae006c6805b547340fc28537f
BayleyEvason <476705@chrome.escg.ac.uk>
2019-11-05 14:29:27 +0000

    put the animations on the movement

--- project/Assets/Playercontroller2D.cs
+++ project/Assets/Playercontroller2D.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playercontroller2D : MonoBehaviour{
+public class Playercontroller2D : MonoBehaviour {
     Animator animator;
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
-    void Start(){
+    void Start() {
 
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
@@ -18,20 +18,24 @@ public class Playercontroller2D : MonoBehaviour{
 
     private void FixedUpdate()
     {
-        if(Input.GetKey("d") || Input.GetKey("right"))
+        if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            rb2d.velocity = new Vector2(2, 0);
+            rb2d.velocity = new Vector2(4, rb2d.velocity.y);
+            animator.Play("player_run");
         }
-        else if(Input.GetKey("a") || Input.GetKey("left"))
+        else if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            rb2d.velocity = new Vector2(-2, 0);
+            rb2d.velocity = new Vector2(-4, rb2d.velocity.y);
+            animator.Play("player_run");
         }
-
-        if(Input.GetKey("space"))
+        else
         {
-            rb2d.velocity = new Vector2(rb2d.velocity.x, 3);
+            animator.Play("player_idle");
+        }
+        if (Input.GetKey("space"))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 5);
+            animator.Play("player_jump");
         }
     }
-
-
 }
